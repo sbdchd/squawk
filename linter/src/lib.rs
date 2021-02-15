f0337305d660ff3c139dfa637b58a1eb64a1ddf8
Christopher Dignam <chris@dignam.xyz>
2021-02-14 20:11:12 -0500

    .

--- linter/src/lib.rs
+++ linter/src/lib.rs
@@ -215,7 +215,7 @@ lazy_static! {
                 ViolationMessage::Note(
                     "Requires a table scan and a SHARE ROW EXCLUSIVE lock on both tables, which blocks writes while the table is scanned.".into()
                 ),
-                ViolationMessage::Help("Add NOT VALID to the constraint and then VALIDATE the constraint.".into()),
+                ViolationMessage::Help("Add NOT VALID to the constraint in one transaction and then VALIDATE the constraint in a separate transaction.".into()),
             ]
         }
     ];
