7cf7112df8555e6df9b03ed79f4010ca624174bc
Christopher Dignam <chris@dignam.xyz>
2021-02-16 23:47:58 -0500

    .

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,10 +1,18 @@
 module.exports = {
   someSidebar: {
     General: ["quick_start", "install", "safe_migrations", "cli", "github_app"],
-    // TODO: Add docs for each rule.
     Rules: [
+      "adding-field-with-default",
+      "adding-foreign-key-constraint",
       "adding-not-nullable-field",
+      "adding-serial-primary-key-field",
+      "ban-char-field",
+      "ban-drop-database",
       "changing-column-type",
+      "constraint-missing-not-valid",
+      "disallowed-unique-constraint",
+      "prefer-robust-stmts",
+      "prefer-text-field",
       "renaming-column",
       "renaming-table",
       "require-concurrent-index-creation",
