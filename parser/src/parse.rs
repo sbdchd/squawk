2105c74f9dc3efebc5bab4767023bcc08dde8dce
Christopher Dignam <chris@dignam.xyz>
2022-01-07 18:33:22 -0500

    fix(parser): parsing alter constraint (#173)
    Location isn't always available
    
    fixes #171

--- parser/src/parse.rs
+++ parser/src/parse.rs
@@ -1232,6 +1232,17 @@ ALTER TABLE public.tasks VALIDATE CONSTRAINT tasks_fk;
         assert!(res.is_ok());
         assert_debug_snapshot!(res);
     }
+    #[test]
+    fn parse_alter_constraint_regression() {
+        let sql = r#"
+ALTER TABLE "table" ALTER CONSTRAINT "constraint" DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "table" ALTER CONSTRAINT "constraint" NOT DEFERRABLE;
+"#;
+        let res = parse_sql_query(sql);
+        assert!(res.is_ok());
+        assert_debug_snapshot!(res);
+    }
 
     #[test]
     fn test_parse_create_table_regression() {
