5d32e52940928903a51e3e55eb03649ad400f9d0
Christopher Dignam <chris@dignam.xyz>
2022-03-27 19:27:24 -0400

    show rules on homepage (#194)

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -17,7 +17,8 @@ const features = [
     title: "GitHub Integration",
     description: (
       <>
-        Use the <Link to={"/docs/github_app"}>Squawk GitHub App</Link> to lint your pull requests.
+        Use the <Link to={"/docs/github_app"}>Squawk GitHub App</Link> to lint
+        your pull requests.
       </>
     ),
   },
@@ -46,6 +47,91 @@ function Feature({ imageUrl, title, description }) {
     </div>
   )
 }
+
+const rules = [
+  {
+    name: "adding-field-with-default",
+    tags: ["locking"],
+    description:
+      "Prevent blocking reads/writes to table while table is rewritten on PG < 11.",
+  },
+  {
+    name: "adding-foreign-key-constraint",
+    tags: ["locking"],
+    description:
+      "Prevent blocking writes to tables while verifying foreign key constraint.",
+  },
+  {
+    name: "adding-not-nullable-field",
+    tags: ["locking"],
+    description:
+      "Prevent blocking reads/writes to table while table is scanned on PG < 11.",
+  },
+  {
+    name: "adding-serial-primary-key-field",
+    tags: ["locking"],
+    description: "Prevent blocking reads/writes to table while index is built.",
+  },
+  {
+    name: "ban-char-field",
+    tags: ["schema"],
+    description: "Prevent mistaken use of character type in schema.",
+  },
+  {
+    name: "ban-drop-database",
+    tags: ["backwards compatibility"],
+    description:
+      "Prevent breaking existing clients that depend on the database.",
+  },
+  {
+    name: "changing-column-type",
+    tags: ["backwards compatibility", "locking"],
+    description:
+      "Prevent breaking existing clients that depend on column type. Prevent blocking reads/writes to table while table is rewritten.",
+  },
+  {
+    name: "constraint-missing-not-valid",
+    tags: ["locking"],
+    description: "Prevent blocking writes to the table while the scan occurs.",
+  },
+  {
+    name: "disallowed-unique-constraint",
+    tags: ["locking"],
+    description: "Prevent blocking reads/writes to table while index is built.",
+  },
+  {
+    name: "prefer-robust-stmts",
+    tags: ["locking"],
+    description: "Ensure migrations are atomic or retriable.",
+  },
+  {
+    name: "prefer-text-field",
+    tags: ["locking"],
+    description:
+      "Prevent blocking reads and writes to table while table metadata is updated.",
+  },
+  {
+    name: "renaming-column",
+    tags: ["backwards compatibility"],
+    description: "Prevent breaking existing clients that depend on column.",
+  },
+  {
+    name: "renaming-table",
+    tags: ["backwards compatibility"],
+    description: "Prevent breaking existing clients that depend on table.",
+  },
+  {
+    name: "require-concurrent-index-creation",
+    tags: ["locking"],
+    description: "Prevent blocking writes to table while index is created.",
+  },
+  {
+    name: "require-concurrent-index-deletion",
+    tags: ["locking"],
+    description:
+      "Prevent blocking reads/writes to table while index is dropped.",
+  },
+]
 
 function Home() {
   const context = useDocusaurusContext()
@@ -77,6 +163,46 @@ function Home() {
                   <Feature key={idx} {...props} />
                 ))}
               </div>
+              <div className="row" />
+              <div className="row">
+                <div className="col">
+                  <a href="/docs/rules">
+                    <h3 style={{ color: "var(--ifm-font-color-base)" }}>
+                      Rules
+                    </h3>
+                  </a>
+                  {[
+                    { title: "Prevent schema mistakes", tags: ["schema"] },
+                    {
+                      title: "Make backwards compatible schema changes",
+                      tags: ["backwards compatibility"],
+                    },
+                    { title: "Apply schema changes safely", tags: ["locking"] },
+                  ].map((sec) => (
+                    <>
+                      <h4 style={{marginBottom: '0.5rem'}}>{sec.title}</h4>
+                      <table style={{marginBottom: '2rem'}}>
+                        <tr>
+                          <th>rule name</th>
+                          <th>description</th>
+                        </tr>
+                        {rules
+                          .filter((rule) =>
+                            sec.tags.some((tag) => rule.tags.includes(tag))
+                          )
+                          .map((rule) => (
+                            <tr key={rule.name}>
+                              <td style={{ wordBreak: "keep-all" }}>
+                                <a href={"/docs/" + rule.name}>{rule.name}</a>
+                              </td>
+                              <td>{rule.description}</td>
+                            </tr>
+                          ))}
+                      </table>
+                    </>
+                  ))}
+                </div>
+              </div>
             </div>
           </section>
         )}
@@ -86,3 +212,4 @@ function Home() {
 }
 
 export default Home
+
