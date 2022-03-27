3be43d9cc3e47a8199f474c4ce14c6db87affa63
Christopher Dignam <chris@dignam.xyz>
2022-03-27 16:49:16 -0400

    show rules on homepage

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -77,6 +77,32 @@ function Home() {
                   <Feature key={idx} {...props} />
                 ))}
               </div>
+              <div className="row">
+              <div class="col">
+    
+
+              <h3>Rules</h3>
+                  <table>
+      <tr><th>rule</th><th>kind</th><th>description</th></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>adding-field-with-default</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>{"Prevent blocking reads/writes to table while table is rewritten on PG < 11."}</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>adding-foreign-key-constraint</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>Prevent blocking writes to tables while verifying foreign key constraint.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>adding-not-nullable-field</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>{"Prevent blocking reads/writes to table while table is scanned on PG < 11."}</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>adding-serial-primary-key-field</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>Prevent blocking reads/writes to table while index is built.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>ban-char-field</td><td>schema</td><td style={{whiteSpace: "nowrap"}}>Prevent mistaken use of character type in schema.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>ban-drop-database</td><td style={{whiteSpace: "nowrap"}}>backwards compatability</td><td>Prevent breaking existing clients that depend on the database.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>changing-column-type</td><td style={{whiteSpace: "nowrap"}}>backwards compatability, locking</td><td>Prevent breaking existing clients that depend on column type. Prevent blocking reads/writes to table while table is rewritten.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>constraint-missing-not-valid</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>Prevent blocking writes to the table while the scan occurs.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>disallowed-unique-constraint</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>Prevent blocking reads/writes to table while index is built.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>prefer-robust-stmts</td><td style={{whiteSpace: "nowrap"}}>migrations safety</td><td>Ensure migrations are atomic or retriable.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>prefer-text-field</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>Prevent blocking reads and writes to table while table metadata is updated.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>renaming-column</td><td style={{whiteSpace: "nowrap"}}>backwards compatability</td><td>Prevent breaking existing clients that depend on column.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>renaming-table</td><td style={{whiteSpace: "nowrap"}}>backwards compatability</td><td>Prevent breaking existing clients that depend on table.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>require-concurrent-index-creation</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>Prevent blocking writes to table while index is created.</td></tr>
+      <tr><td style={{wordBreak: "keep-all"}}>require-concurrent-index-deletion</td><td style={{whiteSpace: "nowrap"}}>locking</td><td>Prevent blocking reads/writes to table while index is dropped.</td></tr>
+
+                  </table>
+</div>
+              </div>
             </div>
           </section>
         )}
