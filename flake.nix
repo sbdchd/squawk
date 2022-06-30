ce2008fd9c514a2ae94c79af55a74d3935bee7d5
Jade Lovelace <jadel@mercury.com>
2022-06-30 11:10:50 -0700

    Nixify
    - Update libpg_query-sys to upstream plus
      https://github.com/tdbgamer/libpg_query-sys/pull/4, which adds a
      LIBPG_QUERY environment variable to use a prebuilt libpg_query
    
      I am following up upstream in nixpkgs to obsolete the
      allowUnsupportedSystem override
    - Add a flake.nix file allowing people to build or get a development
      environment with Nix on macOS and Linux (tested on aarch64)
    
    The practical benefit of this PR is that it is now easier to include
    squawk in Nix-based development environments if desired.
    
    It can still be used as previously as well; this is fully optional.

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Linter for PostgreSQL, focused on migrations";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          # libpg_query has systems = x86_64, which is probably a bug
+          config.allowUnsupportedSystem = true;
+          overlays = [ overlay ];
+        };
+        overlay = (final: prev:
+          let inherit (prev) lib;
+          in
+          {
+            squawk = final.rustPlatform.buildRustPackage {
+              pname = "squawk";
+              version = "0.13.2";
+
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "libpg_query-sys-0.2.0" = "sha256-Txllwr/aZFrrRC+Me+ofX/B9l7JpX6MBLEs2NX2FFqw=";
+                };
+              };
+
+              src = ./.;
+
+              nativeBuildInputs = with final; [
+                pkg-config
+                rustPlatform.bindgenHook
+              ];
+
+              buildInputs = with final; [
+                libiconv
+                openssl
+              ] ++ lib.optionals final.stdenv.isDarwin (with final.darwin.apple_sdk.frameworks; [
+                CoreFoundation
+                Security
+              ]);
+
+              LIBPG_QUERY = final.libpg_query;
+
+              meta = with lib; {
+                description = "Linter for PostgreSQL, focused on migrations";
+                homepage = "https://github.com/sbdchd/squawk";
+                license = licenses.gpl3Only;
+                platforms = platforms.all;
+              };
+            };
+          });
+      in
+      {
+        packages = {
+          squawk = pkgs.squawk;
+        };
+        defaultPackage = self.packages.${system}.squawk;
+        checks = self.packages;
+
+        # for debugging
+        inherit pkgs;
+
+        devShell = pkgs.squawk;
+      });
+}
