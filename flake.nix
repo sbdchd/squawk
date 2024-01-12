03baf9c3083ebaa3e381fb04d9d8d3a0a5f28a22
Christopher Dignam <chris@dignam.xyz>
2024-01-11 23:37:45 -0500

    bump version

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
           {
             squawk = final.rustPlatform.buildRustPackage {
               pname = "squawk";
-              version = "0.26.0";
+              version = "0.27.0";
 
               cargoLock = {
                 lockFile = ./Cargo.lock;
