d98f9766d812b8cf3bd4ed93af37710c17a1454f
Christopher Dignam <chris@dignam.xyz>
2024-06-11 09:37:43 -0400

    .

---
+++ cli/src/file_finding.rs
@@ -0,0 +1,58 @@
+use std::path::PathBuf;
+
+use log::info;
+
+use crate::subcommand::SquawkError;
+
+#[derive(Debug)]
+pub enum FindFilesError {
+    PatternError(glob::PatternError),
+}
+
+impl std::fmt::Display for FindFilesError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Self::PatternError(ref err) => {
+                write!(f, "Failed to build pattern: {err}")
+            }
+        }
+    }
+}
+
+impl std::convert::From<glob::PatternError> for FindFilesError {
+    fn from(e: glob::PatternError) -> Self {
+        Self::PatternError(e)
+    }
+}
+
+/// Given a list of patterns or paths, along with exclusion patterns, find matching files.
+pub fn find_paths(path_patterns: &[String], exclude_patterns: &[String]) -> Vec<PathBuf> {
+    let mut matched_paths = vec![];
+    let exclude_paths: Vec<_> = exclude_patterns
+        .iter()
+        .map(|x| glob::Pattern::new(x).unwrap())
+        .collect();
+    for path in path_patterns
+        .iter()
+        .flat_map(|x| glob::glob(x).unwrap().filter_map(Result::ok))
+    {
+        if path.is_dir() {
+            info!("skipping directory path: {}", path.display());
+            continue;
+        }
+        if let Some(pattern) = exclude_paths
+            .iter()
+            .find(|&excluded| excluded.matches(path.to_str().unwrap()))
+        {
+            info!(
+                "skipping excluded file path: {}. pattern: {}",
+                path.display(),
+                pattern
+            );
+
+            continue;
+        }
+        matched_paths.push(path);
+    }
+    matched_paths
+}
