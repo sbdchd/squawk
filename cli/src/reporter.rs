sbdchd/squawk

commit 0917fe45fc68bfe0863bfe73551a122143d6e433

parser: bump lib_pg parser dep to postgres 16 (#393)
allows parsing things like:


```sql
select and
```

parser/Cargo.toml
@@ -12,10 +12,10 @@ keywords = ["postgres", "sql", "parser"]
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
-libpg_query-sys = { git = "https://github.com/chdsbd/libpg_query-sys.git", rev = "f4584dcbcb8c1f3bee550477257e84a846fdd92d" }
 serde_json = "1.0"
 serde = { version = "1.0", features = ["derive"] }
 serde_repr = "0.1"
+libpg_query-sys = "0.4.0"
 
 [dev-dependencies]
 insta = "0.16.0"

commit 546ae73db74b7da43c1d87c7e50ab20f49e4c585

fix warnings for generated columns (#397)
fixes #396

reverts 0917fe45fc68bfe0863bfe73551a122143d6e433, which broke the build

cli/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "squawk"
-version = "1.4.0"
+version = "1.5.0"
 authors = ["Steve Dignam <steve@dignam.xyz>"]
 edition = "2018"
 license = "GPL-3.0"

cli/src/reporter.rs
@@ -481,10 +481,10 @@ fn get_sql_file_content(violation: &ViolationContent) -> Result<String, std::io:
 
     let violation_content = if violation_count > 0 {
         format!(
-            r#"
+            r"
 ```
 {}
-```"#,
+```",
             violations_text.trim_matches('\n')
         )
     } else {
@@ -494,7 +494,7 @@ fn get_sql_file_content(violation: &ViolationContent) -> Result<String, std::io:
     let violations_emoji = get_violations_emoji(violation_count);
 
     Ok(format!(
-        r#"
+        r"
 <h3><code>{filename}</code></h3>
 
 ```sql
@@ -506,7 +506,7 @@ fn get_sql_file_content(violation: &ViolationContent) -> Result<String, std::io:
 {violation_content}
     
 ---
-    "#,
+    ",
         violations_emoji = violations_emoji,
         filename = violation.filename,
         sql = sql,
@@ -521,7 +521,7 @@ pub fn get_comment_body(files: &[ViolationContent], version: &str) -> String {
     let violations_emoji = get_violations_emoji(violations_count);
 
     format!(
-        r#"
+        r"
 # Squawk Report
 
 ### **{violations_emoji} {violation_count}** violations across **{file_count}** file(s)
@@ -532,7 +532,7 @@ pub fn get_comment_body(files: &[ViolationContent], version: &str) -> String {
 [📚 More info on rules](https://github.com/sbdchd/squawk#rules)
 
 ⚡️ Powered by [`Squawk`](https://github.com/sbdchd/squawk) ({version}), a linter for PostgreSQL, focused on migrations
-"#,
+",
         violations_emoji = violations_emoji,
         violation_count = violations_count,
         file_count = files.len(),

flake.nix
@@ -18,7 +18,7 @@
           {
             squawk = final.rustPlatform.buildRustPackage {
               pname = "squawk";
-              version = "1.4.0";
+              version = "1.5.0";
 
               cargoLock = {
                 lockFile = ./Cargo.lock;

linter/src/rules/adding_field_with_default.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use crate::{
     versions::Version,
-    violations::{RuleViolation, RuleViolationKind},
+    violations::{RuleViolation, RuleViolationKind, ViolationMessage},
 };
 
 use serde_json::{json, Value};
@@ -71,6 +71,20 @@ pub fn adding_field_with_default(
                                         RuleViolationKind::AddingFieldWithDefault,
                                         raw_stmt.into(),
                                         None,
+                                    ));
+                                }
+                                if constraint.contype == ConstrType::Generated {
+                                    errs.push(RuleViolation::new(
+                                        RuleViolationKind::AddingFieldWithDefault,
+                                        raw_stmt.into(),
+                                        Some(vec![
+                                            ViolationMessage::Note(
+                                            "Adding a generated column requires a table rewrite with an ACCESS EXCLUSIVE lock.".into(),
+                                        ),
+                                        ViolationMessage::Help(
+                                            "Add the column as nullable, backfill existing rows, and add a trigger to update the column on write instead.".into(),
+                                        ),
+                                        ]),
                                     ));
                                 }
                             }
@@ -239,5 +253,15 @@ alter table account_metadata add column blah integer default 2 + 2;
 ";
         let pg_version_11 = Some(Version::from_str("11.0.0").unwrap());
         assert_debug_snapshot!(lint_sql(ok_sql, pg_version_11));
+    }
+
+    #[test]
+    fn test_generated_stored() {
+        let bad_sql = r"
+        ALTER TABLE foo
+    ADD COLUMN bar numeric GENERATED ALWAYS AS (bar + baz) STORED;
+        ";
+        let pg_version_11 = Some(Version::from_str("11.0.0").unwrap());
+        assert_debug_snapshot!(lint_sql(bad_sql, pg_version_11));
     }
 }

linter/src/rules/adding_required_field.rs
@@ -4,6 +4,14 @@ use crate::violations::{RuleViolation, RuleViolationKind};
 use squawk_parser::ast::{
     AlterTableCmds, AlterTableDef, AlterTableType, ColumnDefConstraint, ConstrType, RawStmt, Stmt,
 };
+fn has_generated_constraint(constraints: &[ColumnDefConstraint]) -> bool {
+    for ColumnDefConstraint::Constraint(constraint) in constraints {
+        if constraint.contype == ConstrType::Generated {
+            return true;
+        }
+    }
+    false
+}
 
 fn has_not_null_and_no_default_constraint(constraints: &[ColumnDefConstraint]) -> bool {
     let mut has_not_null = false;
@@ -33,6 +41,9 @@ pub fn adding_required_field(
                 for AlterTableCmds::AlterTableCmd(cmd) in &stmt.cmds {
                     if cmd.subtype == AlterTableType::AddColumn {
                         if let Some(AlterTableDef::ColumnDef(column_def)) = &cmd.def {
+                            if has_generated_constraint(&column_def.constraints) {
+                                continue;
+                            }
                             if has_not_null_and_no_default_constraint(&column_def.constraints) {
                                 errs.push(RuleViolation::new(
                                     RuleViolationKind::AddingRequiredField,
@@ -85,5 +96,21 @@ ALTER TABLE "recipe" ADD COLUMN "public" boolean NOT NULL DEFAULT true;
 ALTER TABLE "recipe" ADD COLUMN "public" boolean NOT NULL;
   "#;
         assert_debug_snapshot!(lint_sql(bad_sql));
+    }
+    #[test]
+    fn test_generated_stored_not_null() {
+        let ok_sql = r"
+ ALTER TABLE foo
+    ADD COLUMN bar numeric GENERATED ALWAYS AS (bar + baz) STORED NOT NULL;
+        ";
+        assert_debug_snapshot!(lint_sql(ok_sql));
+    }
+    #[test]
+    fn test_generated_stored() {
+        let ok_sql = r"
+ ALTER TABLE foo
+    ADD COLUMN bar numeric GENERATED ALWAYS AS (bar + baz) STORED ;
+        ";
+        assert_debug_snapshot!(lint_sql(ok_sql));
     }
 }

linter/src/rules/snapshots/squawk_linter__rules__adding_field_with_default__test_rules__generated_stored.snap
@@ -1,0 +1,23 @@
+---
+source: linter/src/rules/adding_field_with_default.rs
+expression: "lint_sql(bad_sql, pg_version_11)"
+---
+[
+    RuleViolation {
+        kind: AddingFieldWithDefault,
+        span: Span {
+            start: 0,
+            len: Some(
+                90,
+            ),
+        },
+        messages: [
+            Note(
+                "Adding a generated column requires a table rewrite with an ACCESS EXCLUSIVE lock.",
+            ),
+            Help(
+                "Add the column as nullable, backfill existing rows, and add a trigger to update the column on write instead.",
+            ),
+        ],
+    },
+]

linter/src/rules/snapshots/squawk_linter__rules__adding_required_field__test_rules__generated_stored.snap
@@ -1,0 +1,5 @@
+---
+source: linter/src/rules/adding_required_field.rs
+expression: lint_sql(ok_sql)
+---
+[]

linter/src/rules/snapshots/squawk_linter__rules__adding_required_field__test_rules__generated_stored_not_null.snap
@@ -1,0 +1,5 @@
+---
+source: linter/src/rules/adding_required_field.rs
+expression: lint_sql(ok_sql)
+---
+[]

parser/Cargo.toml
@@ -15,7 +15,7 @@ keywords = ["postgres", "sql", "parser"]
 serde_json = "1.0"
 serde = { version = "1.0", features = ["derive"] }
 serde_repr = "0.1"
-libpg_query-sys = "0.4.0"
+libpg_query-sys = { git = "https://github.com/chdsbd/libpg_query-sys.git", rev = "f4584dcbcb8c1f3bee550477257e84a846fdd92d" }
 
 [dev-dependencies]
 insta = "0.16.0"

rust-toolchain.toml
@@ -1,3 +1,3 @@
 [toolchain]
 targets = ["x86_64-unknown-linux-gnu", "x86_64-apple-darwin", "aarch64-apple-darwin"]
-channel = "1.80.1"
+channel = "1.84.0"