c8a52e0e74112e7368849591760508820e8bafad
Christopher Dignam <chris@dignam.xyz>
2025-01-11 12:43:51 -0500

    use ubuntu 22 (#401)
    fix #400

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## v1.5.2 - 2025-01-11
+
+### Fixed
+
+- Fixed accidental glibc upgrade (#401).
+
 ## v1.5.1 - 2025-01-10
 
 ### Fixed
--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
           {
             squawk = final.rustPlatform.buildRustPackage {
               pname = "squawk";
-              version = "1.5.1";
+              version = "1.5.2";
 
               cargoLock = {
                 lockFile = ./Cargo.lock;
