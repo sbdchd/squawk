c656b95a2381d64d0f6e6208003ca572db36c245
Steve Dignam <steve@dignam.xyz>
2025-07-12 13:14:47 -0400

    fix

--- example.sql
+++ example.sql
@@ -9,9 +9,6 @@ CREATE TABLE "core_bar" (
 
 CREATE INDEX "field_name_idx" ON "table_name" ("field_name");
 
-ALTER TABLE table_name 
-  ADD CONSTRAINT 
-    field_name_constraint 
-      UNIQUE (field_name);
+ALTER TABLE table_name ADD CONSTRAINT field_name_constraint UNIQUE (field_name);
 
 COMMIT;
