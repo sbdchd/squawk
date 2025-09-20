5cc9d4850e9b69dbf30d941004f879b4a52114b5
Steve Dignam <steve@dignam.xyz>
2025-09-20 19:08:59 -0400

    fix maybe?

--- crates/squawk/tests/example_output.rs
+++ crates/squawk/tests/example_output.rs
@@ -6,6 +6,7 @@ fn example_sql_svg() {
     let bin_path = snapbox::cmd::cargo_bin("squawk");
     Command::new(bin_path)
         .env("CLICOLOR_FORCE", "1")
+        .env("SQUAWK_DISABLE_GITHUB_ANNOTATIONS", "1")
         .arg("../../example.sql")
         .assert()
         .code(1) // squawk returns 1 when it finds violations
