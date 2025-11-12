cbb279cbfe005797f8c6f27fd5681a49d4399693
Steve Dignam <steve@dignam.xyz>
2025-11-11 23:20:34 -0500

    parser: improve create function & create foreign table robustness

--- crates/squawk_parser/tests/data/err/create_function.sql
+++ crates/squawk_parser/tests/data/err/create_function.sql
@@ -3,6 +3,12 @@ create function f()
 returns table (a text  b int)
 as '' language sql;
 
+create function foo(arg float = 1 int4 = 4)
+-- missing comma                 ^
+returns void
+as ''
+language sql;
+
 -- regression partial definition
 create function
 
