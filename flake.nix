067500c3815898500087343fe31cbdecd939613e
Steve Dignam <steve@dignam.xyz>
2026-03-19 21:06:14 -0400

    release: 2.44.0

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
           {
             squawk = final.rustPlatform.buildRustPackage {
               pname = "squawk";
-              version = "2.43.0";
+              version = "2.44.0";
 
               cargoLock = {
                 lockFile = ./Cargo.lock;
