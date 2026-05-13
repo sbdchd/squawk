531907bf257883fe2b968d834336d4be12b0f51f
Steve Dignam <steve@dignam.xyz>
2026-05-12 23:37:24 -0400

    update snap baseline

--- crates/squawk_syntax/test_data/validation/validate_string_continuation.sql
+++ crates/squawk_syntax/test_data/validation/validate_string_continuation.sql
@@ -25,14 +25,18 @@ select e'hello' /* comment */ 'bar' /* another comment */ ' world';
 select e'hello' -- comment
 'world';
 select e'foo' 'bar' 'buzz';
+select e'foo'
+'\u00';
 
 -- error unicode escape string
-select u'foo' 'bar';
-select u'foo' /* comment */ 'bar';
-select u'hello' /* comment */ 'bar' /* another comment */ ' world';
-select u'hello' -- comment
+select u&'foo' 'bar';
+select u&'foo' /* comment */ 'bar';
+select u&'hello' /* comment */ 'bar' /* another comment */ ' world';
+select u&'hello' -- comment
 'world';
-select u'foo' 'bar' 'buzz';
+select u&'foo' 'bar' 'buzz';
+select u&'foo'
+'\010';
 
 -- error bit string
 select b'01' '10';
@@ -41,6 +45,8 @@ select b'01' /* comment */ '11' /* another comment */ '10';
 select b'111' -- comment
 '10';
 select b'10' '10' '11';
+select b'10'
+'20';
 
 -- error byte string
 select x'0F' '10';
@@ -49,3 +55,5 @@ select x'01' /* comment */ '1F' /* another comment */ '10';
 select x'1F1' -- comment
 '10';
 select x'1F' 'F0' '11';
+select x'1F'
+'G0';
