64884860275ffdc0a7bb5900c8e1c68254c96bc7
Steve Dignam <steve@dignam.xyz>
2026-05-30 00:38:31 -0400

    fix

--- crates/xtask/src/update_version.rs
+++ crates/xtask/src/update_version.rs
@@ -138,7 +138,7 @@ fn update_versions(sh: &Shell, v: &str) -> Result<()> {
         r#"const SQUAWK_USER_AGENT: &str = "squawk/.*""#,
         &format!(r#"const SQUAWK_USER_AGENT: &str = "squawk/{v}""#),
     )?;
-    replace_in_file(sh, "README.md", "rev: .*", &format!("rev: {v}"))?;
+    replace_in_file(sh, "README.md", "rev: .*", &format!("rev: v{v}"))?;
 
     Ok(())
 }
