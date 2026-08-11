8af81f7410c255bf72625e1efdaf19b458757f68
Steve Dignam <steve@dignam.xyz>
2026-08-10 21:43:26 -0400

    add tests

---
+++ crates/squawk_parser/tests/data/ok/select_compound_precedence.sql
@@ -0,0 +1,16 @@
+-- INTERSECT has stronger precedence than UNION
+select 1 intersect select 2 union select 3;
+-- so this is:
+-- (select 1 intersect select 2) union select 3;
+
+select 1 union select 2 intersect select 3;
+-- and this is:
+-- select 1 union (select 2 intersect select 3);
+
+-- UNION and EXCEPT are left associative
+select 1 except select 2 union select 3;
+select 1 union select 2 except select 3;
+
+-- parens override precedence and associativity
+select 1 intersect (select 2 union select 3);
+select 1 union (select 2 union select 3);
