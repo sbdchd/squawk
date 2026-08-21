098bb69b04f2cbc69e6afd5d7ce86116dd630ffb
Steve Dignam <steve@dignam.xyz>
2026-08-20 22:44:53 -0400

    fmt: table names & column names (#1299)

--- crates/squawk_fmt/tests/before/create_table.sql
+++ crates/squawk_fmt/tests/before/create_table.sql
@@ -4,3 +4,26 @@ create table t(a int,b text);
 
 -- users table
 create table users (id int);
+
+-- columns that have various quoting requirements
+create table cols ("left" int, "select" text, "data" int, "Mixed" text, UPPER int, U&"c!006fl" uescape '!' int);
+
+-- table names
+create table PUBLIC.Accounts (id int);
+create table "Public"."Users" (id int);
+create table "foo"."quoted_names" ("data" int, "value" text);
+create table "left" (id int);
+create table "table" (id int);
+create table U&"d\0061t\+000061" (id int);
+create table U&"d!0061tum" uescape '!' (id int);
+create table /* foo */ foo /* bar  */ . /* buzz */ bar (id int);
+
+-- comments inside a name node
+create table U&"d!0061tum" /* mid */ uescape '!' (id int);
+create table t (U&"c!006fl" /* c */ uescape '!' int);
+create table t (U&"c!006fl" uescape /* c */ '!' int);
+
+-- comments inside a path
+create table foo/*a*//*b*/.bar (id int);
+create table foo -- a line comment
+. bar (id int);
--- crates/squawk_fmt/tests/before/select.sql
+++ crates/squawk_fmt/tests/before/select.sql
@@ -8,4 +8,9 @@ select foo as "Quoted Alias" from "Quoted Table";
 
 select 1 as "foo";
 
+-- aliases without an `as` are bare col labels, so keywords stay quoted
+select 1 "foo", 2 "filter", 3 "day", 4 "array", 5 "Mixed";
+
+select 1 as "foo", 2 as "filter", 3 as "day", 4 as "array";
+
 select 1 /*a*/group /* b */by/*c */ 1;
