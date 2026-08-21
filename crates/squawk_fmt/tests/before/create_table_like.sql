4dc804393a6e14c5be1401dbeaf0d337e3dc8df0
Steve Dignam <steve@dignam.xyz>
2026-08-20 23:05:31 -0400

    fmt: create table like (#1300)

---
+++ crates/squawk_fmt/tests/before/create_table_like.sql
@@ -0,0 +1,21 @@
+create table t (LIKE foo);
+create table t (like foo, id int);
+create table t (id int, like foo, like bar);
+
+-- like options
+create table t (like foo INCLUDING ALL);
+create table t (like foo EXCLUDING INDEXES);
+create table t (like foo INCLUDING DEFAULTS EXCLUDING CONSTRAINTS INCLUDING IDENTITY);
+create table t (like foo including comments including compression including generated including statistics including storage);
+
+-- table names
+create table t (like PUBLIC.Accounts);
+create table t (like "Public"."Users" including all);
+create table t (like "foo"."bar");
+create table t (like "select");
+create table t (like U&"d!0061tum" uescape '!' including all);
+
+-- comments
+create table t (like /* a */ foo /* b */ including /* c */ all);
+create table t (like foo -- a line comment
+including all);
